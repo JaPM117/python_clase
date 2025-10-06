a67148e2b56c74d04aa63a2653f8fe68af1d0b5d
JaPM117 <70642590+JaPM117@users.noreply.github.com>
2025-10-06 12:41:59 +0200

    Delete ej9.py

--- ej9.py
+++
@@ -1,5 +0,0 @@
-capital=input("¿Cuánto dinero quieres invertir?")
-interes=input("¿Cuál es el interés?")
-tiempo=input("¿Durante cuánto tiempo? (En años)")
-
-print("Vas a ganar" capital*(interes/100)*tiempo)
