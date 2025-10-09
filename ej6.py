ded4ade6c24eb55c8fc52512a401d4be635a3b9d
JaPM117 <70642590+JaPM117@users.noreply.github.com>
2025-10-09 11:07:32 +0200

    Ejercicio 6 terminado

---
+++ ej6.py
@@ -0,0 +1,9 @@
+n=int(input("Introduce el número para hacer el cálculo"))
+
+if n>0:
+    suma=((n*(n+1))/2)
+
+    print("El resultado es",suma)
+
+else:
+    print("Número no valido. Introduzca un número mayor que 0")
