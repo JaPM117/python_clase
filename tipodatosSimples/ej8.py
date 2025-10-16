6da6fcd8e194d280d0537122c9dfbc6729c227a3
JaPM117 <70642590+JaPM117@users.noreply.github.com>
2025-10-16 09:47:36 +0200

    ej 8 terminado

---
+++ tipodatosSimples/ej8.py
@@ -0,0 +1,7 @@
+n=int(input("Escribe un número entero"))
+m=int(input("Escribe un segundo número entero"))
+
+c=(n//m)
+r=(n%m)
+
+print("El cociente entre tus números",n,"y",m,"es",c,"y el resto es",r)
