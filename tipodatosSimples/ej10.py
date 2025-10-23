cc9958ea3f696dec792edd4870e35101735d2685
JaPM117 <70642590+JaPM117@users.noreply.github.com>
2025-10-23 09:53:08 +0200

    ej 10 terminado

---
+++ tipodatosSimples/ej10.py
@@ -0,0 +1,9 @@
+payasos=int(input("Número de payasos en el último pedido: "))
+munecas=int(input("Número de munecas en el último pedido: "))
+
+peso_p=112
+peso_m=75
+
+total=((payasos*peso_p)+(munecas*peso_m))
+
+print("El peso del paquete es de", total, "gramos")
