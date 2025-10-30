12973a064593ffa99d7880950c2b7bec6fc02421
JaPM117 <70642590+JaPM117@users.noreply.github.com>
2025-10-30 10:25:57 +0100

    ej 9 terminado

---
+++ cadenas/ej9.py
@@ -0,0 +1,5 @@
+fecha=input("Introduce tu fecha de nacimiento con el formato dd/mm/aaaa. ")
+
+array=fecha.split("/")
+
+print("Tu fecha de nacimiento es el día",array[0],"del mes",array[1],"del año",array[2])
