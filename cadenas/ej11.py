b535e39a91aae30369d8803296bfa903fe0cbe07
JaPM117 <javierpema88@gmail.com>
2025-11-06 11:09:40 +0100

    ej 11 terminado

---
+++ cadenas/ej11.py
@@ -0,0 +1,9 @@
+prod=str(input("Introduce el nombre de un producto. "))
+prec=float(input("Introduce el precio del producto. "))
+num=int(input("Introduce el número de unidades. "))
+
+total= prec * num
+
+print(f"PRECIO UNITARIO: {prec:.2f} €")
+print(f"NÚMERO DE UNIDADES:", num)
+print(f"COSTE TOTAL: {total:.2f} €")
