a335d4c9169cbf694e0317e2b5150fee041e9243
JaPM117 <70642590+JaPM117@users.noreply.github.com>
2025-11-13 01:45:04 +0100

    ej 2 terminado

---
+++ condicionales/ej2.py
@@ -0,0 +1,8 @@
+passwd="contraseña"
+verificacion=input("Introduce la contraseña ")
+
+if (passwd.lower()==verificacion.lower()):
+    print("La contraseña es correcta")
+
+else:
+    print("No sabes la contraseña")
